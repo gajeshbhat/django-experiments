8153cc461043ce6dd672a5b9baa2f9931c62769c
Gajesh Bhat <gajeshbht@gmail.com>
2018-07-11 23:39:55 +0530

    Updated Social login code

--- allauthdemo/allauthdemo/settings.py
+++ allauthdemo/allauthdemo/settings.py
@@ -161,3 +161,42 @@ ACCOUNT_ADAPTER = 'allauthdemo.adapter.CustomProcessAdapter'
 
 #Account Signup
 ACCOUNT_FORMS = {'signup': 'allauthdemo.forms.SignupForm',}
+
+#Social Account Settings
+SOCIALACCOUNT_PROVIDERS = {
+    'facebook': {
+        'METHOD': 'oauth2',
+        'SCOPE': ['email', 'public_profile', 'user_friends'],
+        'AUTH_PARAMS': {'auth_type': 'reauthenticate'},
+        'INIT_PARAMS': {'cookie': True},
+        'FIELDS': [
+            'id',
+            'email',
+            'name',
+            'first_name',
+            'last_name',
+            'verified',
+            'locale',
+            'timezone',
+            'link',
+            'gender',
+            'updated_time',
+        ],
+        'EXCHANGE_TOKEN': True,
+        'LOCALE_FUNC': 'path.to.callable',
+        'VERIFIED_EMAIL': False,
+        'VERSION': 'v2.12',
+    },
+     'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    }
+}
+SOCIALACCOUNT_QUERY_EMAIL=ACCOUNT_EMAIL_REQUIRED
+SOCIALACCOUNT_EMAIL_REQUIRED=ACCOUNT_EMAIL_REQUIRED
+SOCIALACCOUNT_STORE_TOKENS=False
