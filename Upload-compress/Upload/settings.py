42288190e29ffcbcf83a3cc7c422f0cd99609b0c
Gajesh Bhat <gajeshbht@gmail.com>
2019-03-10 21:46:32 +0530

    Added venv, removed database, DS_STORE and others.

--- Upload-compress/Upload/settings.py
+++ Upload-compress/Upload/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'SECRET_KEY'
+SECRET_KEY = os.getenv('SCKEY') # Enter your secret key in environment varibale.
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
